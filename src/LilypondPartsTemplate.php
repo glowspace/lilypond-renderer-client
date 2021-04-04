d64278800c14e71305f533e0973c6f3b3ddaa8de
Mira <athes01@gmail.com>
2021-04-04 12:07:28 +0200

    ZPS-394 fixes and improvements

--- src/LilypondPartsTemplate.php
+++ src/LilypondPartsTemplate.php
@@ -26,10 +26,13 @@ class LilypondPartsTemplate extends LilypondSrc
             // include also the SATB parts template files
             $this->withIncludeDirectory('satb_parts');
         }
+
+        $this->withFragmentStub('parts/divider', 'pre-src', ['VAR_DIVIDER_TEXT' => 'ZAČÁTEK NOT']);
+        $this->withFragmentStub('parts/divider', 'post-src', ['VAR_DIVIDER_TEXT' => 'KONEC NOT']);
     }
 
     public function withPart(string $name, string $src, 
-                                $key_major = 'c', $time_signature = '4/4', $end_time_signature = false, $end_key_major = false, 
+                                $key_major = 'c', ?string $end_key_major = null, string $time_signature = '4/4', ?string $end_time_signature = null, 
                                 $break_before = false, $part_transpose = false)
     {
         $partSrc = new LilypondSrc($src);
@@ -37,9 +40,9 @@ class LilypondPartsTemplate extends LilypondSrc
         $partSrc->withFragmentStub('parts/part_header', 'header', [
             'VAR_LILYPOND_VERSION' => $this->config->getLilypondVersion(),
             'VAR_KEY_MAJOR_BEGIN' => $key_major,
-            'VAR_KEY_MAJOR_END' => $end_key_major,
+            'VAR_KEY_MAJOR_END' => $end_key_major ?? false,
             'VAR_TIME_BEGIN' => $time_signature,
-            'VAR_TIME_END' => $end_time_signature
+            'VAR_TIME_END' => $end_time_signature ?? $time_signature
         ])->withFragmentStub('parts/part_footer', 'footer');
 
         // include the part in the final zip
