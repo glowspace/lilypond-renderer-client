ff8bf761d3eb513cf80490aa3306109e8536da41
Mira <athes01@gmail.com>
2021-05-02 17:52:33 +0200

    commenting functions and refactor

--- src/Client.php
+++ src/Client.php
@@ -3,33 +3,48 @@
 namespace ProScholy\LilypondRenderer;
 
 use GuzzleHttp\Client as HttpClient;
-use ZipStream\ZipStream;
-use ZipStream\Option\Archive;
-// // use GuzzleHttp\Exception\RequestException;
-// // use GuzzleHttp\Exception\ClientException;
-
 use Exception;
 
 class Client
 {
     protected $client;
 
-    public function __construct()
+    /**
+     * Create a new instance of the Client. If using Laravel, the $host and $port variables are loaded from the config automatically.
+     *
+     * @param string|null $host
+     * @param string|null $port
+     */
+    public function __construct(?string $host = null, ?string $port = null)
     {
+        if (!function_exists('config')) {
+            if (is_null($host) || is_null($port)) {
+                throw new Exception('Both $host and $port variable need to be defined when no config (Laravel) is available.');
+            }
+        }
+
         $this->client = new HttpClient([
-            'base_uri' => config('lilypond_renderer.host') . ':' . config('lilypond_renderer.port')
+            'base_uri' => $host ?? config('lilypond_renderer.host') . ':' . $port ?? config('lilypond_renderer.port')
         ]);
     }
 
-    private function make(string $recipe, string $contents, $zip = false)
+    /**
+     * Create a 'make' request with given recipe out of the supported recipes.
+     *
+     * @param string $recipe
+     * @param string $contents
+     * @param boolean $zip
+     * @return RenderResult
+     */
+    private function make(string $recipe, string $contents, bool $is_zip = false) : RenderResult
     {
         // todo: catch exceptions
         $response = $this->client->post("make?recipe=$recipe", [
             'multipart' => [
                 [
-                    'name'     => $zip ? 'file_zip' : 'file_lilypond', // input name, needs to stay the same
+                    'name'     => $is_zip ? 'file_zip' : 'file_lilypond', // input name, needs to stay the same
                     'contents' => $contents,
-                    'filename' => $zip ? 'score.zip' : 'score.ly' // doesn't matter
+                    'filename' => $is_zip ? 'score.zip' : 'score.ly' // doesn't matter
                 ]
             ]
         ]);
@@ -38,7 +53,14 @@ class Client
         return new RenderResult($recipe, json_decode($response->getBody()->getContents()));
     }
 
-    public function render($lilypond_src, $recipe) : RenderResult
+    /**
+     *  Render the $lilypond_src with a given $recipe, decides automatically whether to use a zip file or textual file.
+     *
+     * @param string|LilypondSrc $lilypond_src
+     * @param string $recipe
+     * @return RenderResult
+     */
+    public function render($lilypond_src, string $recipe) : RenderResult
     {
         if ($lilypond_src instanceof LilypondSrc && $lilypond_src->hasIncludes()) {
             return $this->renderZip($lilypond_src, $recipe);
@@ -47,7 +69,14 @@ class Client
         return $this->make($recipe, (string)$lilypond_src);
     }
 
-    public function renderZip(LilypondSrc $lilypond_src, $recipe) : RenderResult
+    /**
+     * Render the LilypondSrc with a given $recipe.
+     *
+     * @param LilypondSrc $lilypond_src
+     * @param string $recipe
+     * @return RenderResult
+     */
+    public function renderZip(LilypondSrc $lilypond_src, string $recipe) : RenderResult
     {
         $zipStream = $lilypond_src->getZippedSrcStream();
         $contents = stream_get_contents($zipStream);
@@ -58,23 +87,50 @@ class Client
         return $result;
     }
 
+    /**
+     * Render the $lilypond_src as svg, optionally cropped.
+     *
+     * @param string|LilypondSrc $lilypond_src
+     * @param boolean $crop
+     * @return RenderResult
+     */
     public function renderSvg($lilypond_src, $crop = true) : RenderResult
     {
         return $this->render($lilypond_src, $crop ? 'svgcrop' : 'svg');
     }
 
-    public function getProcessedFile($tmp, $filename) : string
+    /**
+     * Get contents of a processed (output) file (after the render).
+     *
+     * @param string $tmp
+     * @param string $filename
+     * @return string
+     */
+    public function getProcessedFile(string $tmp, string $filename) : string
     {
         $response = $this->client->get("get?dir=$tmp&file=$filename");
 
         return $response->getBody()->getContents();
     }
 
+
+    /**
+     * Get the log file from a rendered result.
+     *
+     * @param RenderResult $res
+     * @return string
+     */
     public function getResultLog(RenderResult $res) : string
     {
         return $this->getProcessedFile($res->getTmp(), 'log.txt');
     }
 
+    /**
+     * Get the primary output file from the result.
+     *
+     * @param RenderResult $res
+     * @return string
+     */
     public function getResultOutputFile(RenderResult $res) : string
     {
         if (!$res->isSuccessful()) {
@@ -84,10 +140,16 @@ class Client
         return $this->getProcessedFile($res->getTmp(), $res->getRecipeOutputFile());
     }
 
+
+    /**
+     * Delete the files that are stored as the result of rendering.
+     *
+     * @param RenderResult $res
+     * @return boolean true if deleting was successful, otherwise false.
+     */
     public function deleteResult(RenderResult $res) : bool
     {
-        $promise = $this->deleteResultAsync($res);
-        $response = $promise->wait();
+        $response = $this->deleteResultAsync($res)->wait();
 
         $success = $response->getBody()->getContents() == "ok\n";
 
@@ -98,7 +160,7 @@ class Client
         return $success;
     }
 
-    public function deleteResultAsync(RenderResult $res)
+    private function deleteResultAsync(RenderResult $res)
     {
         $tmp = $res->getTmp();
         return $this->client->getAsync("del?dir=$tmp");
